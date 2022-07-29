55b16a595d7f169b8f05bee86132ed87e644e85f
jainsparsh5 <20bcs127@iiitdwd.ac.in>
2022-07-30 01:42:33 +0530

    Email formsubmit added

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
-const hamburger = document.querySelector('#hamburger');
-const menu = document.querySelector('#menu');
+const hamburger = document.querySelector("#hamburger");
+const menu = document.querySelector("#menu");
+const moon = document.querySelector("#moon");
+const hLinks = document.querySelectorAll("#hLink");
+const body = document.querySelector("body");
 
-hamburger.addEventListener('click' , () => {
+hamburger.addEventListener("click", () => {
+  menu.classList.toggle("hidden");
+  hamburger.classList.toggle("bg-white");
+});
+
+hLinks.forEach((link) => {
+  link.addEventListener("click", () => {
     menu.classList.toggle("hidden");
-    hamburger.classList.toggle("bg-white")
-})
+    hamburger.classList.toggle("bg-white");
+  });
+});
+
+moon.addEventListener("click", () => {
+  body.classList.toggle("dark");
+});
